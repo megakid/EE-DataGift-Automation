74651be9cdad0912092b57c55548c031ceaec8e1
James Connor <megakid@gmail.com>
2020-10-12 13:59:13 +0100

    Missed files

---
+++ ChromeDriverDownloader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EEDataGift
+{
+    static class ChromeDriverDownloader
+    {
+        public static async Task DownloadChromeDriver(string chromeVersion)
+        {
+            var majorVersion = chromeVersion.Split('.').First();
+            Console.WriteLine($"chrome.exe version {chromeVersion}, major version is {majorVersion}.");
+
+            using var httpClient = new HttpClient();
+            var latestVersion = (await httpClient.GetStringAsync("http://chromedriver.storage.googleapis.com/LATEST_RELEASE_86")).Trim();
+
+            Console.WriteLine($"Latest chromedriver.exe compatible with {majorVersion} is {latestVersion}.");
+
+            if (File.Exists("chromedriver.exe"))
+                File.Delete("chromedriver.exe");
+
+            Console.WriteLine($"Downloading chromedriver.exe {latestVersion}...");
+            const string ChromeDriverDistroFileName = "chromedriver_win32.zip";
+            using (var fs = File.Create(ChromeDriverDistroFileName))
+            {
+                using (var stream = await httpClient.GetStreamAsync($"http://chromedriver.storage.googleapis.com/{latestVersion}/{ChromeDriverDistroFileName}"))
+                {
+                    await stream.CopyToAsync(fs);
+                    await fs.FlushAsync();
+                }
+            }
+
+            ZipFile.ExtractToDirectory(ChromeDriverDistroFileName, ".");
+
+            Console.WriteLine($"Unzipped {latestVersion} of chromedriver.exe to working directory.");
+
+        }
+
+        public static string GetFullPath(string fileName)
+        {
+            if (File.Exists(fileName))
+                return Path.GetFullPath(fileName);
+
+            var values = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in values.Split(Path.PathSeparator))
+            {
+                var fullPath = Path.Combine(path, fileName);
+                if (File.Exists(fullPath))
+                    return fullPath;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ChromeVersionProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace EEDataGift
+{
+    static class ChromeVersionProvider
+    {
+        public static string GetChromeVersion(string providedChromePath)
+        {
+            string chromePath = null;
+            if (File.Exists(providedChromePath))
+            {
+                chromePath = Path.GetFullPath(providedChromePath);
+
+                Console.WriteLine($"Using chrome.exe path provided. {chromePath}");
+            }
+            else
+            {
+                Console.WriteLine($"chrome.exe did not exist at the path provided {providedChromePath}");
+            }
+
+            if (chromePath == null)
+            {
+                Console.WriteLine($"Searching PATH for chrome.exe...");
+
+                chromePath = GetFullPath("chrome.exe");
+            }
+
+            if (chromePath == null)
+            {
+                throw new Exception("chrome.exe not found.");
+            }
+
+            var version = FileVersionInfo.GetVersionInfo(chromePath);
+            return version.ProductVersion;
+        }
+
+        public static string GetFullPath(string fileName)
+        {
+            if (File.Exists(fileName))
+                return Path.GetFullPath(fileName);
+
+            var values = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in values.Split(Path.PathSeparator))
+            {
+                var fullPath = Path.Combine(path, fileName);
+                if (File.Exists(fullPath))
+                    return fullPath;
+            }
+
+            return null;
+        }
+    }
+}
